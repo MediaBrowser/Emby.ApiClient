c68e9b9b016e4158348fd08b216a3e1d1a398767
LukePulverenti <luke.pulverenti@gmail.com>
2013-02-28 18:46:19 -0500

    initial checkin

---
+++ MediaBrowser.ApiInteraction/SerializationFormats.cs
@@ -0,0 +1,18 @@
+﻿
+namespace MediaBrowser.ApiInteraction
+{
+    /// <summary>
+    /// Enum SerializationFormats
+    /// </summary>
+    public enum SerializationFormats
+    {
+        /// <summary>
+        /// The json
+        /// </summary>
+        Json,
+        /// <summary>
+        /// The protobuf
+        /// </summary>
+        Protobuf
+    }
+}
