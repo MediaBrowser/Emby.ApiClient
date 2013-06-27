d55d19c0590c4dec579bd76f58e411cab7469f78
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-06-26 23:23:15 -0400

    add timeout to server locater

--- MediaBrowser.ApiInteraction/ServerLocator.cs
+++ MediaBrowser.ApiInteraction/ServerLocator.cs
@@ -2,19 +2,43 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MediaBrowser.ApiInteraction
 {
     public class ServerLocator
     {
+        public Task<IPEndPoint> FindServer(CancellationToken cancellationToken)
+        {
+            return FindServer(2000, cancellationToken);
+        }
+
+
         /// <summary>
         /// Attemps to discover the server within a local network
         /// </summary>
-        public async Task<IPEndPoint> FindServer()
+        public Task<IPEndPoint> FindServer(int timeout, CancellationToken cancellationToken)
+        {
+            var taskCompletionSource = new TaskCompletionSource<IPEndPoint>();
+
+            var timeoutToken = new CancellationTokenSource(timeout).Token;
+
+            var linkedTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, timeoutToken);
+
+            linkedTokenSource.Token.Register(() => taskCompletionSource.TrySetCanceled());
+
+            FindServer(taskCompletionSource, timeout);
+
+            return taskCompletionSource.Task;
+        }
+
+        private async void FindServer(TaskCompletionSource<IPEndPoint> taskCompletionSource, int timeout)
         {
             // Create a udp client
             var client = new UdpClient(new IPEndPoint(IPAddress.Any, GetRandomUnusedPort()));
+
+            client.Client.ReceiveTimeout = timeout;
 
             // Construct the message the server is expecting
             var bytes = Encoding.UTF8.GetBytes("who is MediaBrowserServer?");
@@ -22,30 +46,40 @@ namespace MediaBrowser.ApiInteraction
             // Send it - must be IPAddress.Broadcast, 7359
             var targetEndPoint = new IPEndPoint(IPAddress.Broadcast, 7359);
 
-            // Send it
+            // Send the broadcast
             await client.SendAsync(bytes, bytes.Length, targetEndPoint).ConfigureAwait(false);
-           
-            // Get a result back
-            var result = await client.ReceiveAsync().ConfigureAwait(false);
 
-            if (result.RemoteEndPoint.Port == targetEndPoint.Port)
+            try
             {
-                // Convert bytes to text
-                var text = Encoding.UTF8.GetString(result.Buffer);
+                // Get a result back
+                var result = await client.ReceiveAsync().ConfigureAwait(false);
 
-                // Expected response : MediaBrowserServer|192.168.1.1:1234
-                // If the response is what we're expecting, proceed
-                if (text.StartsWith("mediabrowserserver", StringComparison.OrdinalIgnoreCase))
+                if (result.RemoteEndPoint.Port == targetEndPoint.Port)
                 {
-                    text = text.Split('|')[1];
+                    // Convert bytes to text
+                    var text = Encoding.UTF8.GetString(result.Buffer);
 
-                    var vals = text.Split(':');
+                    // Expected response : MediaBrowserServer|192.168.1.1:1234
+                    // If the response is what we're expecting, proceed
+                    if (text.StartsWith("mediabrowserserver", StringComparison.OrdinalIgnoreCase))
+                    {
+                        text = text.Split('|')[1];
 
-                    return new IPEndPoint(IPAddress.Parse(vals[0]), int.Parse(vals[1]));
+                        var vals = text.Split(':');
+
+                        var endpoint = new IPEndPoint(IPAddress.Parse(vals[0]), int.Parse(vals[1]));
+
+                        taskCompletionSource.SetResult(endpoint);
+                        return;
+                    }
                 }
-            }
 
-            return null;
+                taskCompletionSource.SetException(new ArgumentException("Unexpected response"));
+            }
+            catch (SocketException ex)
+            {
+                taskCompletionSource.SetException(ex);
+            }
         }
 
         /// <summary>
