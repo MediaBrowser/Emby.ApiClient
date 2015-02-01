e00c2f54d397ebf96b3973facf91170e0ff08861
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-02-01 12:08:34 -0500

    add videos view

--- MediaBrowser.ApiInteraction/Data/LocalItemQuery.cs
+++ MediaBrowser.ApiInteraction/Data/LocalItemQuery.cs
@@ -7,5 +7,12 @@ namespace MediaBrowser.ApiInteraction.Data
         public string AlbumArtist { get; set; }
         public string AlbumId { get; set; }
         public string Type { get; set; }
+        public string MediaType { get; set; }
+        public string[] ExcludeTypes { get; set; }
+
+        public LocalItemQuery()
+        {
+            ExcludeTypes = new string[] { };
+        }
     }
 }
