6474abdfbbc6bfe56d170ddb598c6016286a05b4
Luke Pulverenti <luke.pulverenti@gmail.com>
2017-09-16 12:07:13 -0400

    target .net standard

---
+++ Emby.ApiClient/MyPortableTimer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Emby.ApiClient
+{
+    /// <summary>
+    /// Missing from PCL, except if targeting .NET 4.5.1 + Win8.1 + WP8.1
+    /// </summary>
+    internal sealed class Timer : CancellationTokenSource
+    {
+        internal Timer(Action<object> callback, object state, int millisecondsDueTime, int millisecondsPeriod)
+        {
+            //Contract.Assert(period == -1, "This stub implementation only supports dueTime.");
+
+            Task.Delay(millisecondsDueTime, Token).ContinueWith(async (t, s) =>
+            {
+                var tuple = (Tuple<Action<object>, object>)s;
+
+                while (!IsCancellationRequested)
+                {
+                    Task.Run(() => tuple.Item1(tuple.Item2));
+
+                    await Task.Delay(millisecondsPeriod, Token).ConfigureAwait(false);
+                }
+
+            }, Tuple.Create(callback, state), CancellationToken.None, TaskContinuationOptions.ExecuteSynchronously | TaskContinuationOptions.OnlyOnRanToCompletion, TaskScheduler.Default);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+                Cancel();
+
+            base.Dispose(disposing);
+        }
+    }
+}
